01145f6755d4e6dad1833346de3ec00ec22ac05e
kirin <x6041500@hotmail.com>
2015-12-19 13:59:46 +0800

    create TeamBean.java

---
+++ TwentyWork/src/com/iii/twentywork/model/bean/TeamBean.java
@@ -0,0 +1,27 @@
+package com.iii.twentywork.model.bean;
+
+public class TeamBean {
+	private int teamid;
+	private String teamname;
+	private byte[] teamimage;
+	
+	public int getTeamid() {
+		return teamid;
+	}
+	public void setTeamid(int teamid) {
+		this.teamid = teamid;
+	}
+	public String getTeamname() {
+		return teamname;
+	}
+	public void setTeamname(String teamname) {
+		this.teamname = teamname;
+	}
+	public byte[] getTeamimage() {
+		return teamimage;
+	}
+	public void setTeamimage(byte[] teamimage) {
+		this.teamimage = teamimage;
+	}
+	
+}
