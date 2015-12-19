057045dd7308c6b652e46c8b1461169ee578518d
kirin <x6041500@hotmail.com>
2015-12-19 13:29:35 +0800

    delete test.java create UserBean.java

---
+++ TwentyWork/src/com/iii/twentywork/model/bean/UsersBean.java
@@ -0,0 +1,62 @@
+package com.iii.twentywork.model.bean;
+
+public class UsersBean {
+	private int userid;
+	private String username;
+	private String email;
+	private byte[] password;
+	private java.util.Date birth;
+	private byte[] userimage;
+	private String cellphone;
+	private String phone;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public byte[] getPassword() {
+		return password;
+	}
+	public void setPassword(byte[] password) {
+		this.password = password;
+	}
+	public java.util.Date getBirth() {
+		return birth;
+	}
+	public void setBirth(java.util.Date birth) {
+		this.birth = birth;
+	}
+	public byte[] getUserimage() {
+		return userimage;
+	}
+	public void setUserimage(byte[] userimage) {
+		this.userimage = userimage;
+	}
+	public String getCellphone() {
+		return cellphone;
+	}
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
