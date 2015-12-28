2b71f85972591ad4148d7f95426f76ee1c7e6c0b
RitaHuang <rita.huang.2006@gmail.com>
2015-12-29 03:43:27 +0800

    新增Filter 檢驗多個url pattern

--- TwentyWork/src/com/iii/twentywork/model/misc/LoginFilter.java
+++ TwentyWork/src/com/iii/twentywork/model/misc/LoginFilter.java
@@ -1,6 +1,10 @@
 package com.iii.twentywork.model.misc;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,13 +20,24 @@ import javax.servlet.http.HttpSession;
 import com.iii.twentywork.model.bean.users.UsersBean;
 
 @WebFilter(
-		urlPatterns={"/ShareFile/*"}
+		urlPatterns={"/*"},
+		initParams= {
+		        @WebInitParam(name="url_1" , value="/ShareFile/*"),
+		        @WebInitParam(name="url_2" , value="/main/*")}
 )
 public class LoginFilter implements Filter {
-	
+	private Collection<String> url = new ArrayList<String>();
+    
+    
 	private FilterConfig fConfig;
 	public void init(FilterConfig fConfig) throws ServletException {
 		this.fConfig=fConfig;
+		
+		Enumeration<String> e = fConfig.getInitParameterNames();
+		while(e.hasMoreElements()) {
+		    String name = e.nextElement();
+		    url.add(fConfig.getInitParameter(name));
+		}
 	}
 
     public LoginFilter() { }
@@ -32,17 +48,43 @@ public class LoginFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
 		HttpSession session = request.getSession();
-		UsersBean bean = (UsersBean) session.getAttribute("LoginOK");
-		if(bean==null) {//還沒Login
-			String uri = request.getRequestURI();
-			session.setAttribute("requestURI", uri);
-			String path = request.getContextPath();
-			response.sendRedirect(path+"/login/login.jsp");
-		}else
-		{
-			chain.doFilter(request, response);
+		String servletPath = request.getServletPath();
+		if(mustLogin(servletPath)) {
+    		UsersBean bean = (UsersBean) session.getAttribute("LoginOK");
+    		if(bean==null) {//還沒Login
+    			String uri = request.getRequestURI();
+    			session.setAttribute("requestURI", uri);
+    			String path = request.getContextPath();
+    			response.sendRedirect(path+"/login/login.controller");
+    		}else
+    		{
+    			chain.doFilter(request, response);
+    		}
+		}else {
+		    chain.doFilter(request, response);
 		}
 	}
+	
+	private boolean mustLogin(String servletPath) {
+	    boolean login = false;
+	    for (String sURL : url) {
+	        if(sURL.endsWith("*")) {
+	            sURL = sURL.substring(0, sURL.length()-1);
+	            if(servletPath.startsWith(sURL)) {
+	                login = true;
+	                break;
+	            }
+	        }else {
+	            if(servletPath.equals(sURL)) {
+	                login = true;
+                    break;  
+	            }
+	        }
+	    }
+	    return login;
+	}
+	
+	
 
 
 }
